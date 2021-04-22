728c8caeb1f24d162b59a521333f286b0e2c34fd
baodieppro <69683917+baodieppro@users.noreply.github.com>
2021-04-22 13:13:20 +0700

    Update Presets.swift

--- DNSecure/Models/Presets.swift
+++ DNSecure/Models/Presets.swift
@@ -10,108 +10,44 @@ import Foundation
 enum Presets {
     static let servers: Resolvers = [
         .init(
-            name: "Google Public DNS",
-            configuration: .dnsOverTLS(
-                DoTConfiguration(
-                    servers: [
-                        "8.8.8.8",
-                        "8.8.4.4",
-                        "2001:4860:4860::8888",
-                        "2001:4860:4860::8844",
-                    ],
-                    serverName: "dns.google"
-                )
-            )
-        ),
-        .init(
-            name: "Google Public DNS",
+            name: "Protect you from Malware, Adults & Unsafe things (Cloudflare)",
             configuration: .dnsOverHTTPS(
                 DoHConfiguration(
                     servers: [
-                        "8.8.8.8",
-                        "8.8.4.4",
-                        "2001:4860:4860::8888",
-                        "2001:4860:4860::8844",
+                        "1.1.1.3",
+                        "1.0.0.3",
+                        "2606:4700:4700::1113",
+                        "2606:4700:4700::1003",
                     ],
-                    serverURL: URL(string: "https://dns.google/dns-query")
+                    serverURL: URL(string: "https://family.cloudflare-dns.com/dns-query")
                 )
             )
         ),
         .init(
-            name: "1.1.1.1",
-            configuration: .dnsOverTLS(
-                DoTConfiguration(
-                    servers: [
-                        "1.1.1.1",
-                        "1.0.0.1",
-                        "2606:4700:4700::64",
-                        "2606:4700:4700::6400",
-                    ],
-                    serverName: "cloudflare-dns.com"
-                )
-            )
-        ),
-        .init(
-            name: "1.1.1.1",
+            name: "Block Inapproriate contents & Unsafe Videos on Youtube and Internet (Cleanbrowsing)",
             configuration: .dnsOverHTTPS(
                 DoHConfiguration(
                     servers: [
-                        "1.1.1.1",
-                        "1.0.0.1",
-                        "2606:4700:4700::64",
-                        "2606:4700:4700::6400",
+                        "185.228.168.168",
+                        "185.228.169.168",
+                        "2a0d:2a00:1::",
+                        "2a0d:2a00:2::",
                     ],
-                    serverURL: URL(string: "https://cloudflare-dns.com/dns-query")
+                    serverURL: URL(string: "https://doh.cleanbrowsing.org/doh/family-filter/")
                 )
             )
         ),
         .init(
-            name: "Quad9",
-            configuration: .dnsOverTLS(
-                DoTConfiguration(
-                    servers: [
-                        "9.9.9.9",
-                        "149.112.112.112",
-                        "2620:fe::fe",
-                        "2620:fe::fe:9",
-                    ],
-                    serverName: "dns.quad9.net"
-                )
-            )
-        ),
-        .init(
-            name: "Quad9",
+            name: "Protect parent and kids from threats & bad contents on the Internet (OpenDNS Family Shield)",
             configuration: .dnsOverHTTPS(
                 DoHConfiguration(
                     servers: [
-                        "9.9.9.9",
-                        "149.112.112.112",
-                        "2620:fe::fe",
-                        "2620:fe::fe:9",
+                        "208.67.222.123",
+                        "208.67.220.123",
+                        "::ffff:d043:de7b",
+                        "::ffff:d043:dc7b",
                     ],
-                    serverURL: URL(string: "https://dns.quad9.net/dns-query")
-                )
-            )
-        ),
-        .init(
-            name: "LibreDNS",
-            configuration: .dnsOverHTTPS(
-                DoHConfiguration(
-                    servers: [
-                        "116.202.176.26",
-                    ],
-                    serverURL: URL(string: "https://doh.libredns.gr/dns-query")
-                )
-            )
-        ),
-        .init(
-            name: "LibreDNS (No Ads)",
-            configuration: .dnsOverHTTPS(
-                DoHConfiguration(
-                    servers: [
-                        "116.202.176.26",
-                    ],
-                    serverURL: URL(string: "https://doh.libredns.gr/ads")
+                    serverURL: URL(string: "https://doh.familyshield.opendns.com/dns-query")
                 )
             )
         ),
